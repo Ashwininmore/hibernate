350164bd593346d62227a31d3a1d8a4573bd1b8d
Ashwini Nitin More <69009147+Ashwininmore@users.noreply.github.com>
2023-12-28 19:35:38 +0530

    Table_Per_ConcreteClass

---
+++ Table_Per_Concrete_Class/src/mypack/Main.java
@@ -0,0 +1,35 @@
+package mypack;
+
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Configuration c=new Configuration();
+		c.configure("hibernate.cfg.xml");
+		SessionFactory factory = c.buildSessionFactory();
+		Session session=factory.openSession();
+		Transaction t=session.beginTransaction();
+		
+		Employee e1= new Employee();
+		e1.setName("Ashu");
+		Regular_Emp e2=new Regular_Emp();
+		e2.setName("Swami");
+		e2.setSalary(66000);
+		e2.setBonus(1200);
+		Contract_Emp e3=new Contract_Emp();
+		e3.setName("Aakash");
+		e3.setPay_per_hour(1000);
+		e3.setContract_period("12 months");
+		
+		session.persist(e1);
+		session.persist(e2);
+		session.persist(e3);
+		
+		t.commit();
+		session.close();
+
+	}
+
+}
---
+++ Table_Per_Concrete_Class/src/mypack/Regular_Emp.java
@@ -0,0 +1,22 @@
+package mypack;
+
+public class Regular_Emp extends Employee{
+	int salary, bonus;
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public int getBonus() {
+		return bonus;
+	}
+
+	public void setBonus(int bonus) {
+		this.bonus = bonus;
+	}
+	
+}
